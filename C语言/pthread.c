caac9e56d2b84c279e0a73a74b6b8cea8c694c81
刘铮 <47924501+qq471334212@users.noreply.github.com>
2019-07-08 23:55:49 +0800

    Create pthread.c

---
+++ C语言/pthread.c
@@ -0,0 +1,17 @@
+/ *****************************************
+2019年7月8日23时53分三十三秒
+刘铮
+练习
+******************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+int main(void){
+    int temp1,temp2;
+    void *retval;
+    return 0;
+}
